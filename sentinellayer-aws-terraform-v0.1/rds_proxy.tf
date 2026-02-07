7f520c04052e40356cb2412c394ce6ce039b7244
mrrCarter <carther@bu.edu>
2026-02-06 22:21:26 -0500

    trying to pull from laptop

--- sentinellayer-aws-terraform-v0.1/rds_proxy.tf
+++ sentinellayer-aws-terraform-v0.1/rds_proxy.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role" "rds_proxy" {
+  count = var.enable_rds_proxy ? 1 : 0
+
   name = "${local.name_prefix}-rds-proxy-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -14,8 +16,10 @@ resource "aws_iam_role" "rds_proxy" {
 }
 
 resource "aws_iam_role_policy" "rds_proxy_secrets" {
+  count = var.enable_rds_proxy ? 1 : 0
+
   name = "${local.name_prefix}-rds-proxy-secrets"
-  role = aws_iam_role.rds_proxy.id
+  role = aws_iam_role.rds_proxy[0].id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -29,9 +33,11 @@ resource "aws_iam_role_policy" "rds_proxy_secrets" {
 }
 
 resource "aws_db_proxy" "postgres" {
+  count = var.enable_rds_proxy ? 1 : 0
+
   name                   = "${local.name_prefix}-proxy"
   engine_family          = "POSTGRESQL"
-  role_arn               = aws_iam_role.rds_proxy.arn
+  role_arn               = aws_iam_role.rds_proxy[0].arn
   vpc_subnet_ids         = module.vpc.private_subnets
   vpc_security_group_ids = [aws_security_group.rds_proxy.id]
 
@@ -48,7 +54,9 @@ resource "aws_db_proxy" "postgres" {
 }
 
 resource "aws_db_proxy_default_target_group" "postgres" {
-  db_proxy_name = aws_db_proxy.postgres.name
+  count = var.enable_rds_proxy ? 1 : 0
+
+  db_proxy_name = aws_db_proxy.postgres[0].name
 
   connection_pool_config {
     max_connections_percent      = 90
@@ -58,8 +66,10 @@ resource "aws_db_proxy_default_target_group" "postgres" {
 }
 
 resource "aws_db_proxy_target" "postgres" {
-  db_proxy_name     = aws_db_proxy.postgres.name
-  target_group_name = aws_db_proxy_default_target_group.postgres.name
+  count = var.enable_rds_proxy ? 1 : 0
+
+  db_proxy_name     = aws_db_proxy.postgres[0].name
+  target_group_name = aws_db_proxy_default_target_group.postgres[0].name
   # AWS expects the DB instance *identifier* (lowercase/hyphens), not the internal resource id (db-...).
   db_instance_identifier = aws_db_instance.postgres.identifier
 }
